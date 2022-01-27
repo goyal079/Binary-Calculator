e233e153a358cadc6ce0aba09d20cf3bee2658ff
goyal <goyalhimanshu079@gmail.com>
2022-01-27 20:26:04 +0530

    string to binary

---
+++ converters.js
@@ -0,0 +1,25 @@
+//Primary Binary converters
+function decToBin(num) {
+  if (Math.floor(num / 2) < 2) {
+    return (num % 2) + "" + Math.floor(num / 2);
+  } else {
+    return (num % 2) + "" + decToBin(Math.floor(num / 2));
+  }
+}
+
+function binary(input) {
+  let bin = decToBin(input);
+  return bin.split("").reverse().join("").padStart(8, 0); // returns 8 bit binaries
+}
+
+//Primary Binary converters
+function stringToBinary(string) {
+  let x = string
+    .split("")
+    .map((char) => {
+      return binary(char.charCodeAt(0));
+    })
+    .join("");
+  return x;
+}
+export { binary };
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function decToBin(num) {
-  if (Math.floor(num / 2) < 2) {
-    return (num % 2) + "" + Math.floor(num / 2);
-  } else {
-    return (num % 2) + "" + binaray(Math.floor(num / 2));
-  }
-}
+import { question } from "readline-sync";
+import { binary } from "./converters.js";
+
+// let response = question(
+//   "Select the type of data you want to convert to binary;\n0. Exit\n1. String\n2. Decimal \n3. Octal\n4. HexaDecimal: "
+// );
+
+// switch (response) {
+//   case "0":
+//     console.log("Exiting :(");
+//     break;
+// }
+console.log(binary(17));
